a7389d9077fd79105f7335bb3d89b85d6b8ab509
Rahul Rajput <76735555+RahulRaj352001@users.noreply.github.com>
2022-01-28 17:55:07 +0530

    ok setup

---
+++ src/views/collection/User/User.js
@@ -0,0 +1,110 @@
+import PropTypes from 'prop-types'
+import React, { useEffect, useState, createRef } from 'react'
+import classNames from 'classnames'
+import axios from 'axios'
+import { rgbToHex } from '@coreui/utils'
+import { DocsLink } from 'src/components'
+import {
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CRow,
+  CTable,
+  CTableBody,
+  CTableCaption,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+} from '@coreui/react'
+import { DocsCallout, DocsExample } from 'src/components'
+const ThemeView = () => {
+  const [color, setColor] = useState('rgb(255, 255, 255)')
+  const ref = createRef()
+
+  useEffect(() => {
+    const el = ref.current.parentNode.firstChild
+    const varColor = window.getComputedStyle(el).getPropertyValue('background-color')
+    setColor(varColor)
+  }, [ref])
+
+  return (
+    <table className="table w-100" ref={ref}>
+      <tbody>
+        <tr>
+          <td className="text-medium-emphasis">HEX:</td>
+          <td className="font-weight-bold">{rgbToHex(color)}</td>
+        </tr>
+        <tr>
+          <td className="text-medium-emphasis">RGB:</td>
+          <td className="font-weight-bold">{color}</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+const ThemeColor = ({ className, children }) => {
+  const classes = classNames(className, 'theme-color w-75 rounded mb-3')
+  return (
+    <CCol xs={12} sm={6} md={4} xl={2} className="mb-4">
+      <div className={classes} style={{ paddingTop: '75%' }}></div>
+      {children}
+      <ThemeView />
+    </CCol>
+  )
+}
+
+ThemeColor.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+}
+
+const Colors = () => {
+  const [allusers, setAllusers] = useState([])
+  useEffect(() => {
+    axios.get('localhost:3000/admin/getalluser').then((res) => {
+      setAllusers(res.data)
+      console.log(res.data)
+    })
+  }, [])
+  return (
+    <>
+      <CCol xs={12}>
+        <CCard className="mb-1">
+          <CCardHeader>
+            <strong>EasyWork Users</strong>
+          </CCardHeader>
+          <CCardBody>
+            <div href="components/table#hoverable-rows">
+              <CTable hover className="okok">
+                <CTableHead>
+                  <CTableRow>
+                    <CTableHeaderCell scope="col">#</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Name</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Username</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Email</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Country</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Mobile</CTableHeaderCell>
+                    <CTableHeaderCell scope="col">Settings</CTableHeaderCell>
+                  </CTableRow>
+                </CTableHead>
+                <CTableBody>
+                  <CTableRow>
+                    <CTableHeaderCell scope="row">1</CTableHeaderCell>
+                    <CTableDataCell>Mark</CTableDataCell>
+                    <CTableDataCell>Otto</CTableDataCell>
+                    <CTableDataCell>@mdo</CTableDataCell>
+                  </CTableRow>
+                </CTableBody>
+              </CTable>
+            </div>
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </>
+  )
+}
+
+export default Colors
